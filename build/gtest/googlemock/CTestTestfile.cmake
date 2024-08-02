b3f15721f1c685e26cea193f020a8c0a81aff0d5
Mostafa-Samy-Mohammed <mostafasmy020@gmail.com>
2024-08-02 14:40:51 +0300

    delete build

--- build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/mostafa/real_life_demo_repo/real-life-demo/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
